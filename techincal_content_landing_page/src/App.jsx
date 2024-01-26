0a719398bd45608553b3f584306398ee98e00bc3
Sarang Kharche <sarangkharche@gmail.com>
2024-01-26 00:07:48 +0000

    first commit

---
+++ techincal_content_landing_page/src/App.jsx
@@ -0,0 +1,38 @@
+import styles from "./style";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./component";
+
+const App = () =>
+(
+  // navbar
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+
+  </div>
+);
+
+
+export default App
---
+++ techincal_content_landing_page/src/component/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from '../style';
+import { discount, robot } from '../assets';
+
+const Hero = () => (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+        <div className="flex-1 flex flex-col justify-center items-center md:items-start"> 
+        ></div>
+
+    </section>
+)
+
+export default Hero
