b75045f3eb736e215d743e107340b121e59dfcbe
Sarang Kharche <sarangkharche@gmail.com>
2024-01-28 01:39:14 +0000

    Populated stats component and changed Get Started button color

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,28 +1,25 @@
-import styles from '../style';
-import { article, discount, robot } from '../assets';
-import GetStarted from './GetStarted'; // Import the missing component
+import styles from "../style";
+import { article, discount, robot } from "../assets";
+import GetStarted from "./GetStarted"; // Import the missing component
 
 const Hero = () => (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-
-        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-
+        <div
+            className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+        >
             <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
                 <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
 
-
                 <p className={`${styles.paragraph} ml-2`}>
-                    <span className="text-white">20%</span> discount for {" "}
+                    <span className="text-white">20%</span> discount for{" "}
                     <span className="text-white">1 Month</span> Subscription
                 </p>
-
             </div>
 
             <div className="flex flex-row justify-between items-center w-full">
                 <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[90px] leading-[75px]">
-                    Writing Technical Content <br className="sm:block hidden" /> {" "}
-                    <span className="text-gradient">For You.</span> {" "}
-
+                    Writing Technical Content <br className="sm:block hidden" />{" "}
+                    <span className="text-gradient">For You.</span>{" "}
                 </h1>
 
                 <div className="ss:flex hidden md:mr-4 mr-0">
@@ -30,28 +27,33 @@ const Hero = () => (
                 </div>
             </div>
 
-            <button className="font-poppins font-semibold ss:text-[28px] text-[52px] text-white ss:leading-[50px] leading-[75px] bg-blue-500 hover:bg-blue-600 py-1 px-4 rounded mt-5 mb-5">
+            <button className="font-poppins font-semibold ss:text-[28px] text-[52px] text-white ss:leading-[50px] leading-[75px] bg-blue-700 hover:bg-blue-800 py-1 px-4 rounded mt-5 mb-5">
                 Get Started
             </button>
 
             <p className={`${styles.paragraph} max-w-[470px] mt-5}`}>
-                At Whyphy Teh, we specialize in breaking down intricate technical concepts into simple, easy to understand, and relatable content.
+                At Whyphy Teh, we specialize in breaking down intricate technical
+                concepts into simple, easy to understand, and relatable content.
             </p>
         </div>
 
-        <div>
-            <img src={article} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+            <img
+                src={article}
+                alt="billing"
+                className="w-[100%] h-[100%] relative z-[5]"
+            />
             <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
             {/* changed to blue from white  */}
             <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 blue__gradient" />
             {/* changed to black from blue   */}
             <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 black__gradient" />
-
-
-
         </div>
 
+        <div className={`ss:hidden ${styles.flexCenter}`}>
+            <GetStarted />
+        </div>
     </section>
-)
+);
 
-export default Hero
+export default Hero;
--- src/constants/index.js
+++ src/constants/index.js
@@ -73,18 +73,18 @@ export const feedback = [
 export const stats = [
     {
         id: "stats-1",
-        title: "User Active",
-        value: "3800+",
+        title: "Tech Contents Created",
+        value: "200+",
     },
     {
         id: "stats-2",
-        title: "Trusted by Company",
-        value: "230+",
+        title: "Categories of Content",
+        value: "6+",
     },
     {
         id: "stats-3",
-        title: "Transaction",
-        value: "$230M+",
+        title: "Views",
+        value: "500k+",
     },
 ];
 
